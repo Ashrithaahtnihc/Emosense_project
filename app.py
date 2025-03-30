1a4795d28e64a34156fa7f71f29bfcde338494bb
Ashrithaahtnihc <ashrithachintha5104@gmail.com>
2025-03-30 21:04:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -529,4 +529,4 @@ with open("runtime.txt", "w") as f:
     f.write("python-3.10.12")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
